2bd8d214bb37b8220227ef6c5e822d7fba297767
Nathan Nichols <nnichols@concentricsky.com>
2020-04-01 00:09:34 -0700

    Add configuration for mac os

---
+++ fresh.sh
@@ -0,0 +1,12 @@
+# Install homebrew (will also download xcode command line tools)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install GUI apps
+brew cask install google-chrome slack skype vlc numi zoomus iterm2 transmission protonvpn rectangle firefox kap homebrew/cask-versions/docker-edge
+
+# Install CLI apps
+brew install tmux nvim fzf
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
---
+++ install.sh
@@ -0,0 +1,3 @@
+ln -s "$(pwd)"/.zshrc ~/.zshrc
+ln -s "$(pwd)"/.vimrc ~/.vimrc
+ln -s "$(pwd)"/.tmux.conf ~/.tmux.conf
