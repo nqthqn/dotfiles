1a54543292a15da07f805fb4108c182eb8bcf7d1
Nathan Nichols <nnichols@concentricsky.com>
2020-04-08 19:56:31 -0700

    numi is paid app, use bat (cat with syntax highlighting) and fd

--- fresh.sh
+++ fresh.sh
@@ -2,10 +2,10 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install GUI apps
-brew cask install google-chrome clipy slack skype vlc numi zoomus iterm2 transmission protonvpn rectangle firefox kap homebrew/cask-versions/docker-edge
+brew cask install google-chrome clipy slack skype vlc zoomus iterm2 transmission protonvpn rectangle firefox kap homebrew/cask-versions/docker-edge
 
 # Install CLI apps
-brew install tmux nvim fzf node
+brew install tmux nvim fzf node bat fd
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
