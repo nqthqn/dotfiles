61fb650eb53ccc318a5268095c0769756d726e63
Mr. N <hi@nqthqn.com>
2021-11-11 21:26:08 -0800

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -2,13 +2,10 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install GUI apps
-brew install --cask google-chrome clipy slack skype vlc zoomus iterm2 transmission protonvpn rectangle firefox kap homebrew/cask-versions/docker-edge
+brew install --cask clipy zoomus iterm2 protonvpn rectangle firefox
 
 # Install CLI apps
-brew install tmux nvim fzf node bat fd
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install tmux fzf node
 
 # Use pure prompt
 npm install --global pure-prompt
